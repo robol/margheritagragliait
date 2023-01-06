74c04f39f302e1e1ad583147dda7b02c57503638
Emilia-Rodriguez-Gimenez <e.gimenezrodriguez@doveconviene.com>
2023-01-06 15:22:21 +0100

    hero placeholder

---
+++ src/components/hero-banner.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const HeroBanner = () => {
+
+  return (
+      {/*     <StaticImage //TODO: EVALUATE THIS
+      src="../images/icon-m.png"
+      loading="eager"
+      width={64}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt=""
+      style={{ marginBottom: `var(--space-3)` }}
+    /> */}
+  )
+}
+
+export default HeroBanner
