0a45af935daf8a963612d2631160b77e92400fa1
Emilia-Rodriguez-Gimenez <e.gimenezrodriguez@doveconviene.com>
2023-03-03 13:57:03 +0100

    hero

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -1,36 +1,24 @@
 import * as React from "react"
+import * as styles from "../components/index.module.css"
 
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 const HeroBanner = () => {
   return (
-    <div
-      style={{
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        maxHeight: "500px",
-        overflow: "hidden",
-      }}
-    >
-      <StaticImage //TODO: EVALUATE THIS
-        src="../images/placeholder-background.png"
+    <div className={styles.heroBanner}>
+      {/*       <StaticImage //TODO: EVALUATE THIS
+        src="../images/placeholder-background.jpg"
         loading="eager"
         quality={95}
         objectFit="cover"
         formats={["auto", "webp", "avif"]}
         alt=""
         style={{ marginBottom: `var(--space-3)`, width: " 100%" }}
-      />
-      <h1
-        style={{
-          position: "absolute",
-        }}
-      >
-        MARGHERITA GRAGLIA
-      </h1>
+      /> */}
+      <div className={styles.typewriter} style={{}}>
+        <h1>MARGHERITA GRAGLIA</h1>
+      </div>
     </div>
   )
 }
