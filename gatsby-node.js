dcd1cf4a36b5d39916f17e3117efb36662861b52
emirodriguezalc <e.gimenezrodriguez@doveconviene.com>
2023-06-22 06:27:00 +0200

    Add redirect rules for pages ending in .html

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,19 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions;
+
+  // Create page for "/using-dsg"
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
-  })
-}
+  });
+
+  // Redirect URLs ending in ".html" to "/"
+  createRedirect({
+    fromPath: "/*.html",
+    toPath: "/",
+    redirectInBrowser: true,
+    isPermanent: true,
+  });
+};
