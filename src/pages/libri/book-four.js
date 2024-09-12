31243a9dac731076b053a4020ccc7b0f8c6c5d18
emirodriguezalc <e.gimenezrodriguez@doveconviene.com>
2024-09-12 11:24:37 +0200

    Fix: Breaking file structure

--- src/pages/libri/book-four.js
+++ src/pages/libri/book-four.js
@@ -50,7 +50,7 @@ const BookFour = () => (
           Attraverso i dati delle ricerche più recenti e i contributi di autorevoli esperti, il libro fornisce un’analisi approfondita dell’incongruenza di genere. Inoltre, offre <strong>strumenti pratici per gli interventi educativi, per la presa in carico psicologica</strong> e per la promozione dell’inclusione istituzionale e sociale.
           <br />
           <br />
-		  Rivolto a psicologi, operatori sanitari, educatori, insegnanti e genitori, come pure a chiunque sia interessato all’argomento, <strong>il testo accosta, in modo originale, prospettive cliniche e pedagogiche a un approccio multidisciplinare</strong>, presentando una visione integrata degli aspetti significativi che gli adolescenti con incongruenza di genere e gender variant affrontano nel percorso di riconoscimento e affermazione della propria identità.
+          Rivolto a psicologi, operatori sanitari, educatori, insegnanti e genitori, come pure a chiunque sia interessato all’argomento, <strong>il testo accosta, in modo originale, prospettive cliniche e pedagogiche a un approccio multidisciplinare</strong>, presentando una visione integrata degli aspetti significativi che gli adolescenti con incongruenza di genere e gender variant affrontano nel percorso di riconoscimento e affermazione della propria identità.
         </p>
         <div style={{ display: "flex", flexDirection: "column" }}>
           <h3
@@ -60,7 +60,7 @@ const BookFour = () => (
           </h3>
           <div style={{ display: "flex" }}>
             <a
-              href={                "https://www.amazon.it/Lincongruenza-genere-adolescenza-Margherita-Graglia/dp/8874669968/ref=sr_1_1?dib=eyJ2IjoiMSJ9.oAwbcWtDxzcFng13lIB-m7-A1neiTFY75qpkq0EXbYZ0mapeUc3fW8VWR7Lfi59RXjOKUsEydwV2-9vc75acHg.GfzszsW6hiiFS-C2e3FEgK9vIQH7DE5dBEewqymgC78&dib_tag=se&keywords=graglia+margherita&qid=1725642954&sr=8-1"
+              href={"https://www.amazon.it/Lincongruenza-genere-adolescenza-Margherita-Graglia/dp/8874669968/ref=sr_1_1?dib=eyJ2IjoiMSJ9.oAwbcWtDxzcFng13lIB-m7-A1neiTFY75qpkq0EXbYZ0mapeUc3fW8VWR7Lfi59RXjOKUsEydwV2-9vc75acHg.GfzszsW6hiiFS-C2e3FEgK9vIQH7DE5dBEewqymgC78&dib_tag=se&keywords=graglia+margherita&qid=1725642954&sr=8-1"
               }
               target="_blank" rel="noreferrer"
             >
@@ -114,9 +114,9 @@ const BookFour = () => (
       <h3>
         Introduzione <br />
       </h3>
-	  <h3>
+      <h3>
         Parte prima<br />
-		Aspetti teorici<br />
+        Aspetti teorici<br />
       </h3>
       <div style={{ marginLeft: "15px" }}>
         1. Dal passato al presente<br /> <br />
@@ -124,22 +124,24 @@ const BookFour = () => (
         3. Il percorso di affermazione di genere. di <em>Davide Dettore</em> e <em>Francesca Mazzoli</em> <br /> <br />
         4. Il genere. Strumento di conoscenza e campo di conflitto sociale. di <em>Chiara Bertone</em> <br /> <br />
         5. Transnegatività <br /> <br />
-	  <h3>
-        Parte seconda<br />
-		Aspetti applicativi<br />
-      </h3>
-      <div style={{ marginLeft: "15px" }}>
-        6. Contesti di sviluppo<br /> <br />
-        7. Incongruenza di genere e neurodiversità. di <em>Angela Caldarera</em> <br /> <br />
-        8. Promuovere l’inclusione istituzionale <br /> <br />
-        9. La co-costruzione del genere nei contesti educativi 0-6 anni. di <em>Alessandra Braglia</em> e <em>Elena Maccaferri</em> <br /> <br />
-        10. Controversie e conflitti etici nell’incongruenza di genere: una disamina bioetica. di <em>Elena Nave</em> e <em>Matteo Cresti</em> <br /> <br />
-        <strong>Conclusioni</strong>
-        <strong>Bibliografia</strong>
-        <strong>Materiali on line</strong>
-		<div style={{ marginLeft: "15px" }}>
-        11. La presa in carico psicologica di <em>Chiara Baietto</em><br /> <br />
-        12. Corpo a corpo. L’identità di genere del terapeuta. di <em>Julien Montecchi</em> <br /> <br />
+        <h3>
+          Parte seconda<br />
+          Aspetti applicativi<br />
+        </h3>
+        <div style={{ marginLeft: "15px" }}>
+          6. Contesti di sviluppo<br /> <br />
+          7. Incongruenza di genere e neurodiversità. di <em>Angela Caldarera</em> <br /> <br />
+          8. Promuovere l’inclusione istituzionale <br /> <br />
+          9. La co-costruzione del genere nei contesti educativi 0-6 anni. di <em>Alessandra Braglia</em> e <em>Elena Maccaferri</em> <br /> <br />
+          10. Controversie e conflitti etici nell’incongruenza di genere: una disamina bioetica. di <em>Elena Nave</em> e <em>Matteo Cresti</em> <br /> <br />
+          <strong>Conclusioni</strong>
+          <strong>Bibliografia</strong>
+          <strong>Materiali on line</strong>
+          <div style={{ marginLeft: "15px" }}>
+            11. La presa in carico psicologica di <em>Chiara Baietto</em><br /> <br />
+            12. Corpo a corpo. L’identità di genere del terapeuta. di <em>Julien Montecchi</em> <br /> <br />
+          </div>
+        </div>
       </div>
     </p>
   </Layout>
